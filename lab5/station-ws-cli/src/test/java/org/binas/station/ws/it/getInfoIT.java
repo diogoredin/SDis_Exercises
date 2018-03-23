854181c8e3338d70322d2a77e2372452d976e6fb
Diogo Redin <diogo.redin@gmail.com>
2018-03-23 20:50:02 +0000

    getInfoTest base

---
+++ lab5/station-ws-cli/src/test/java/org/binas/station/ws/it/getInfoIT.java
@@ -0,0 +1,16 @@
+package org.binas.station.ws.it;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Class that tests getBina operation
+ */
+public class getInfoIT extends BaseIT {
+
+	@Test
+	public void getInfoTest() {
+		//Assert.notNull(client.getInfo());
+	}
+
+}
